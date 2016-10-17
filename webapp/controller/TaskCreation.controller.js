9cee60cf1e96dad1e8b0490ab62977c2399f596f
Armbrust Samuel <samuel.armbrust@sap.com>
2016-10-17 20:27:02 +0000

    Task creation controller complete

---
+++ webapp/controller/TaskCreation.controller.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("SapTaskTrackerApp.controller.TaskCreation", {
+
+		sDestinationURL: "tasktrackerfiori",
+
+		onSaveClicked: function() {
+			var sInputValue = this.getView().byId("taskNameInput").getValue();
+			var oTask = {
+				name: sInputValue
+			};
+
+			this.fnSaveTaskOnServer(oTask, this);
+		},
+
+		onBackClicked: function() {
+			this.getView().byId("taskNameInput").setValue("");
+			sap.ui.getCore().byId("app").back();
+		},
+
+		fnSaveTaskOnServer: function(oTask, oController) {
+			jQuery.ajax(this.sDestinationURL, {
+				dataType: "json",
+				data: JSON.stringify(oTask),
+				method: "POST",
+				contentType: "application/json; charset=UTF-8",
+				success: function() {
+					oController.getView().byId("taskNameInput").setValue("");
+					sap.ui.getCore().byId("app").back();
+				}
+			});
+		}
+
+	});
+
+});
